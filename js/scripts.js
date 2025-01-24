01f5590d92f3de38f59035f046cef39dc25f8bd8
Mark Lund <marklund117@gmail.com>
2025-01-24 12:32:47 -0700

    mobile navigation with layering

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ console.log('JavaScript is running.')
 // add correct year to footer
 const rightNow = new Date()
 document.querySelector('#year').textContent = rightNow.getFullYear()
+
+// navigation stuff
+const navElement = document.querySelector('#theNav')
+const btnElement = document.querySelector('#theButton')
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open')
+    navElement.classList.toggle('open')
+})
